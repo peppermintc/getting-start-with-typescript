1e573cf03da04883b255298126a4b22345551d11
peppermintc <haminjp@naver.com>
2020-07-28 04:50:25 +0900

    log out

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -5,14 +5,27 @@ var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
 router.get("/login", function (req, res) {
-    res.send("\n    <form method=\"POST\">\n        <div>\n            <label>Email</label>\n            <input name=\"em\" />\n        </div>\n        <div>\n            <label>Password</label>\n            <input name=\"pa\" type=\"password\" />\n        </div>\n        <button>Submit</button>\n    </form>\n  ");
+    res.send("\n    <form method=\"POST\">\n        <div>\n            <label>Email</label>\n            <input name=\"email\" />\n        </div>\n        <div>\n            <label>Password</label>\n            <input name=\"password\" type=\"password\" />\n        </div>\n        <button>Submit</button>\n    </form>\n  ");
 });
 router.post("/login", function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    if (email) {
-        res.send(email.toUpperCase());
+    if (email && password && email === "hi@hi.com" && password === "password") {
+        req.session = { loggedIn: true };
+        res.redirect("/");
     }
     else {
-        res.send("You must provide an email property");
+        res.send("Invalid email or password");
     }
+});
+router.get("/", function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n         <div>\n            <div>You are logged in</div>\n            <a href=\"/logout\">Logout</a>\n         </div>\n         ");
+    }
+    else {
+        res.send("\n         <div>\n            <div>You are not logged in</div>\n            <a href=\"/login\">Login</a>\n         </div>\n         ");
+    }
+});
+router.get("/logout", function (req, res) {
+    req.session = null;
+    res.redirect("/");
 });
