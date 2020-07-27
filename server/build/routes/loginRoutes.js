bbd41c7ab58986525c4c8c0dd1d34f912310abac
peppermintc <haminjp@naver.com>
2020-07-28 04:04:40 +0900

    express + typescript

---
+++ server/build/routes/loginRoutes.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+var express_1 = require("express");
+var router = express_1.Router();
+exports.router = router;
+router.get("/login", function (req, res) {
+    res.send("\n    <form method=\"POST\">\n        <div>\n            <label>Email</label>\n            <input name=\"em\" />\n        </div>\n        <div>\n            <label>Password</label>\n            <input name=\"pa\" type=\"password\" />\n        </div>\n        <button>Submit</button>\n    </form>\n  ");
+});
+router.post("/login", function (req, res) {
+    var _a = req.body, email = _a.email, password = _a.password;
+    if (email) {
+        res.send(email.toUpperCase());
+    }
+    else {
+        res.send("You must provide an email property");
+    }
+});
---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,31 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="em" />
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="pa" type="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send("You must provide an email property");
+  }
+});
+
+export { router };
